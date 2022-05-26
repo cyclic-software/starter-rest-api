e511991d89c9bfdb7669f9f9e6dc623dda64f73c
Kam Lasater <ckl@seekayel.com>
2022-05-25 21:25:08 -0400

    Added create end point

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('cyclic-dynamodb')
 
 app.use(helmet())
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended: true}))
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
@@ -22,8 +22,21 @@ app.use(express.urlencoded())
 // app.use(express.static('public', options))
 // #############################################################################
 
+app.post('/:col/:key', async (req, res) => {
+
+  console.log(req.body)
+
+  const col = req.params.col
+  const key = req.params.key
+  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+  const item = await db.collection(col).set(key, req.body)
+  console.log(JSON.stringify(item, null, 2))
+  res.json(item).end()
+})
+
+
 app.delete('/:col/:key', async (req, res) => {
-  let col = req.params.col
+  const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
   const item = await db.collection(col).delete(key)
@@ -32,7 +45,7 @@ app.delete('/:col/:key', async (req, res) => {
 })
 
 app.get('/:col/:key', async (req, res) => {
-  let col = req.params.col
+  const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
   const item = await db.collection(col).get(key)
