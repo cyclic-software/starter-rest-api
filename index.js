15dd651334ebcec55b1d2581242786a886464f4f
Kam Lasater <ckl@seekayel.com>
2022-05-25 20:30:08 -0400

    Added eslint

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const helmet = require("helmet");
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded())
 
 // #############################################################################
 // This configures static hosting for files in /public that have the extensions
