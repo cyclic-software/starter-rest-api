2767c0e75daab8bd82864b9eb21ab1cc2d1046d5
Kam Lasater <ckl@seekayel.com>
2022-05-25 20:24:01 -0400

    Added express

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log(process.env)
+const express = require("express");
+const helmet = require("helmet");
+
+const app = express();
+
+app.use(helmet());
+
+// #############################################################################
+// This configures static hosting for files in /public that have the extensions
+// listed in the array.
+// var options = {
+//   dotfiles: 'ignore',
+//   etag: false,
+//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
+//   index: ['index.html'],
+//   maxAge: '1m',
+//   redirect: false
+// }
+// app.use(express.static('public', options))
+// #############################################################################
+
+// Catch all handler for all other request.
+app.use('*', (req,res) => {
+  res.json({"msg":"no route handler found"}).end()
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`index.js listening on ${port}`)
+})
