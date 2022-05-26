ffe4a9bbed7f70a98b067a237ae5ae1e56e0fc44
Kam Lasater <ckl@seekayel.com>
2022-05-25 21:46:12 -0400

    Added json schema validation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const helmet = require('helmet')
+const validate = require('express-jsonschema').validate
 
 const db = require('cyclic-dynamodb')
 
@@ -22,7 +23,23 @@ app.use(express.urlencoded({ extended: true }))
 // app.use(express.static('public', options))
 // #############################################################################
 
-app.post('/:col/:key', async (req, res) => {
+const petSchema = {
+  id: '/Pet',
+  type: 'object',
+  properties: {
+    gender: {
+      type: 'string',
+      required: true,
+      enum: ['male', 'female']
+    },
+    breed: {
+      type: 'string',
+      required: true
+    }
+  }
+}
+
+app.post('/:col/:key', validate({ body: petSchema }), async (req, res) => {
   console.log(req.body)
 
   const col = req.params.col
@@ -62,6 +79,48 @@ app.get('/:col', async (req, res) => {
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
   res.json({ msg: 'no route handler found' }).end()
+})
+
+/*
+    Setup a general error handler for JsonSchemaValidation errors.
+    As mentioned before, how one handles an invalid request depends on their application.
+    You can easily create some express error middleware
+    (http://expressjs.com/guide/error-handling.html) to customize how your
+    application behaves. When the express-jsonschema.validate middleware finds invalid data it
+    passes an instance of JsonSchemaValidation to the next middleware.
+    Below is an example of a general JsonSchemaValidation error handler for
+    an application.
+*/
+app.use(function (err, req, res, next) {
+  let responseData
+
+  if (err.name === 'JsonSchemaValidation') {
+    // Log the error however you please
+    console.log(err.message)
+    // logs "express-jsonschema: Invalid data found"
+
+    // Set a bad request http response status or whatever you want
+    res.status(400)
+
+    // Format the response body however you want
+    responseData = {
+      statusText: 'Bad Request',
+      jsonSchemaValidation: true,
+      validations: err.validations // All of your validation information
+    }
+
+    // Take into account the content type if your app serves various content types
+    if (req.xhr || req.get('Content-Type') === 'application/json') {
+      res.json(responseData)
+    } else {
+      // If this is an html request then you should probably have
+      // some type of Bad Request html template to respond with
+      res.render('badrequestTemplate', responseData)
+    }
+  } else {
+    // pass error to next error middleware handler
+    next(err)
+  }
 })
 
 const port = process.env.PORT || 3000
