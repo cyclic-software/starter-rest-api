736b5aa2a958c4e7703223f36bdeaa23ac5dd715
Kam Lasater <ckl@seekayel.com>
2022-05-25 21:00:58 -0400

    Added get item

--- index.js
+++ index.js
@@ -22,11 +22,22 @@ app.use(express.urlencoded())
 // app.use(express.static('public', options))
 // #############################################################################
 
+
+app.get('/:col/:key', async (req,res) => {
+  let col = req.params.col
+  let key = req.params.key
+  console.log(`from collection: ${col} get key: ${key} with params ${req.params}`)
+  let item = await db.collection(col).get(key)
+  console.log(JSON.stringify(item,null,2))
+  res.json(item).end()
+})
+
 app.get('/:col', async (req,res) => {
-  console.log(req.params)
-  let items = await db.collection(req.params['col']).list()
+  let col = req.params.col
+  console.log(`list collection: ${col} with params: ${req.params}`)
+  let items = await db.collection(col).list()
   console.log(JSON.stringify(items,null,2))
-  res.json(items)
+  res.json(items).end()
 })
 
 // Catch all handler for all other request.
