79a0c1775b3ae298e4ea3688fd79bb7810015f00
wallabyway <bealester@gmail.com>
2022-10-09 18:06:51 -0700

    fixed to work with latest dynoDB sdk and vsCode

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const CyclicDb = require('cyclic-dynamodb')
+const CyclicDb = require('cyclic-dynamodb');
+const db = CyclicDb(process.env.CYCLIC_DB);
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -33,9 +34,8 @@ app.post('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const coldb = db.collection(col);
-  const item = await coldb.set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
+  const item = await db.collection(col).set(key, req.body)
+  console.log(item)
   res.json(item).end()
 })
 
@@ -45,7 +45,7 @@ app.delete('/:col/:key', async (req, res) => {
   const key = req.params.key
   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
   const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
+  console.log(item)
   res.json(item).end()
 })
 
@@ -54,10 +54,8 @@ app.get('/:col/:key', async (req, res) => {
   const col = req.params.col
   const key = req.params.key
   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`);
-  const db = new CyclicDb("red-cockroach-tuxCyclicDB");
-  const coldb = db.collection(col);
-  const item = await coldb.get(key)
-  console.log(JSON.stringify(item, null, 2))
+  const item = await db.collection(col).get(key)
+  console.log(item)
   res.json(item).end()
 })
 
@@ -71,9 +69,7 @@ app.get('/:col', async (req, res) => {
 })
 
 // Catch all handler for all other request.
-app.use('*', (req, res) => {
-  res.json({ msg: 'no route handler found' }).end()
-})
+app.use(express.static('www'));
 
 // Start the server
 const port = process.env.PORT || 3000
