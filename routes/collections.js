75fc334ccde9681ccc80da6de189257b6233b0d9
Ismail Vega <ismail.softing@gmail.com>
2023-03-13 14:19:56 -0300

    feat(API): add api route
    Signed-off-by: Ismail Vega <ismail.softing@gmail.com>

--- routes/collections.js
+++ routes/collections.js
@@ -2,15 +2,15 @@ const router = require("express").Router();
 const controller = require("../controllers/collections");
 
 // Create or Update a collection
-router.route("/:col/:key").post(controller.setCollection);
+router.route("/api/:col/:key").post(controller.setCollection);
 
 // Delete a collection
-router.route("/:col/:key").delete(controller.deleteCollection);
+router.route("/api/:col/:key").delete(controller.deleteCollection);
 
 // Get a single collection
-router.route("/:col/:key").get(controller.getCollection);
+router.route("/api/:col/:key").get(controller.getCollection);
 
 // Get a full listing of collections
-router.route("/:col").get(controller.getCollections);
+router.route("/api/:col").get(controller.getCollections);
 
 module.exports = router;
